cc5034708bd7390a3fc7aaa2912da9ca9cb652d0
ahmed-shehab-444 <444050140@std.psau.edu.sa>
2026-05-02 17:01:44 +0300

    Update SchedulerSimulationSync.java

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -84,9 +84,12 @@ class SharedResources {
     
     // Method to log execution
     public static void logExecution(String message) {
-        // TODO: Protect this critical section with a lock
-        // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
+         logLock.lock();
+        try {
+            executionLog.add(message);
+        } finally {
+            logLock.unlock();
+        }
     }
 }
 
